c7248c1d453a813175bda4a052272d1692d12c97
AnilPonnaganti <133189615+AnilPonnaganti@users.noreply.github.com>
2023-05-11 14:47:51 +0530

    Initial commit

---
+++ WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyNamespace.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetStudents()
+        {
+            var students = new List<Student>
+            {
+                new Student
+                {
+                    Name = "Alice",
+                    Age = 20,
+                    Hobbies = new List<string> { "reading", "swimming", "coding" }
+                },
+                new Student
+                {
+                    Name = "Bob",
+                    Age = 22,
+                    Hobbies = new List<string> { "painting", "dancing", "singing" }
+                }
+            };
+
+            return Ok(new { students });
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Hobbies { get; set; }
+    }
+}
